f7aef8f5b8edc8198c9cde775941e8688a3217ce
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-11-04 01:56:16 +0000

    Configurable 0, 90, 180 and 270 degree rotation [1/2]
    Change-Id: Ia1859c51d71ef9d01cec2d13c6468ed89c6ac53e
    Contributor: jonasl
    
    - Add system setting
    
    Author: Tim Schumacher <timschumi@gmx.de>
    Date:   Wed Nov 28 21:44:18 2018 +0100
    
        Add back ACCELEROMETER_ROTATION_ANGLES and update references
    
        This needed to move back into fw/b, because it needs to be
        accessed from inside the RotationPolicy. Previously
        (when this constant and Setting were located in lineage-sdk),
        the settings for the display rotation had no effect, because we
        couldn't query the correct set of settings.
    
        Change-Id: Icce249925a578c328db3884e5d332b20d6e7db6c
        Fixes: BUGBASH-2042
    
    Author: eray orçunus <erayorcunus@gmail.com>
    Date:   Mon Jun 22 22:47:40 2015 +0300
    
        Rotation related corrections
    
        - There are some conditions deleted while placing rotation angles code, I added them.
    
        - Rotation lock was screwed up since CM 12. Fixed it by fetching allowed rotations from CM's
        allowed rotations setting.
    
        - Also, a CAF commit had killed rotation lock ability.
    
        [port to 15.1]:
         - ACCELEROMETER_ROTATION_ANGLES moved to LineageSDK
         - Slight change of the WindowManager API
    
        [port to 16.0]:
         - adjust context
         - ACCELEROMETER_ROTATION_ANGLES moved to Settings
         - Use the configstore API
    
        Change-Id: I8f1b468249c68e7b6514d1a96bdb3fc638af84fd
        Signed-off-by: eray orçunus <erayorcunus@gmail.com>
        (cherry picked from commit a62720d51e9d8f3a3c9992ea0bdb707b64e865b5)
    
    Author: Tim Schumacher <timschumi@gmx.de>
    Date:   Thu May 2 19:48:39 2019 +0200
    
        RotationPolicy: Don't crash if configstore 1.1 isn't available
    
        Change-Id: I77301ec8c72393daa0003ca310eee07b767d4e69
    
    Change-Id: Ia7bf8cb64258e1d602230a8f9ea227d3b56a4dab

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4833,6 +4833,23 @@ public final class Settings {
                 new SettingsValidators.InclusiveIntegerRangeValidator(5, 60);
 
         /**
+         * Control the type of rotation which can be performed using the accelerometer
+         * if ACCELEROMETER_ROTATION is enabled.
+         * Value is a bitwise combination of
+         * 1 = 0 degrees (portrait)
+         * 2 = 90 degrees (left)
+         * 4 = 180 degrees (inverted portrait)
+         * 8 = 270 degrees (right)
+         * Setting to 0 is effectively orientation lock
+         * @hide
+         */
+        public static final String ACCELEROMETER_ROTATION_ANGLES = "accelerometer_rotation_angles";
+
+        /** @hide */
+        public static final Validator ACCELEROMETER_ROTATION_ANGLES_VALIDATOR =
+                NON_NEGATIVE_INTEGER_VALIDATOR;
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
@@ -5059,6 +5076,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(INCREASING_RING);
             PRIVATE_SETTINGS.add(INCREASING_RING_START_VOLUME);
             PRIVATE_SETTINGS.add(INCREASING_RING_RAMP_UP_TIME);
+            PRIVATE_SETTINGS.add(ACCELEROMETER_ROTATION_ANGLES);
         }
 
         /**
@@ -5187,6 +5205,7 @@ public final class Settings {
             VALIDATORS.put(INCREASING_RING, INCREASING_RING_VALIDATOR);
             VALIDATORS.put(INCREASING_RING_START_VOLUME, INCREASING_RING_START_VOLUME_VALIDATOR);
             VALIDATORS.put(INCREASING_RING_RAMP_UP_TIME, INCREASING_RING_RAMP_UP_TIME_VALIDATOR);
+            VALIDATORS.put(ACCELEROMETER_ROTATION_ANGLES, ACCELEROMETER_ROTATION_ANGLES_VALIDATOR);
         }
 
         /**
