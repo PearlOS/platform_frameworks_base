4d7ba50864f732e9983577ae7095342bb4b90f14
bigrushdog <bigrushdog@users.noreply.github.com>
2020-05-30 15:30:04 +0000

    base: Utils: Add API to take screenshots
    abc: extracted from DUI
    add a public call to Utils

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -5816,6 +5816,11 @@ public class WindowManagerService extends IWindowManager.Stub
     }
 
     @Override
+    public void sendCustomAction(Intent intent) {
+        mPolicy.sendCustomAction(intent);
+    }
+
+    @Override
     public void lockNow(Bundle options) {
         mPolicy.lockNow(options);
     }
